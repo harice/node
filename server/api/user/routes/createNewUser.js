55351c4bd0d505b6a5b179deb5351a36587f79f7
John <glian.panugaling@gmail.com>
2016-06-22 11:01:41 +0800

    added new fields in user model

--- server/api/user/routes/createNewUser.js
+++ server/api/user/routes/createNewUser.js
@@ -16,6 +16,15 @@ export default {
         lastName: Joi.string().required(),
         email: Joi.string().email().required(),
         password: Joi.string().required(),
+        phoneNumber: Joi.string().optional(),
+        company: Joi.string().optional(),
+        gender: Joi.string().optional(),
+        address1: Joi.string().optional(),
+        address2: Joi.string().optional(),
+        city: Joi.string().optional(),
+        state: Joi.string().optional(),
+        country: Joi.string().optional(),
+        zipcode: Joi.string().optional()
       }
     },
     plugins: {
@@ -30,20 +39,26 @@ export default {
       const { User } = request.models;
       const { convertValidationErrors } = request.server.plugins.common;
 
-      const u = User.build({
+      User.build({
         firstName: request.payload.firstName,
         lastName: request.payload.lastName,
         email: request.payload.email,
-        password: request.payload.password
-      });
-
-      u.save()
-        .then(savedUser => {
-          return savedUser.sanitizeForResponse();
-        })
-        .catch(Sequelize.ValidationError, convertValidationErrors)
-        .asCallback(reply);
-
+        password: request.payload.password,
+        phoneNumber: request.payload.phoneNumber,
+        gender: request.payload.gender,
+        company: request.payload.company,
+        address1: request.payload.address1,
+        address2: request.payload.address2,
+        city: request.payload.city,
+        state: request.payload.state,
+        country: request.payload.country,
+        zipcode: request.payload.zipcode
+      }).save()
+      .then(savedUser => {
+        return savedUser.sanitizeForResponse();
+      })
+      .catch(Sequelize.ValidationError, convertValidationErrors)
+      .asCallback(reply);
     }
   }
 }
